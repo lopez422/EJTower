2c7cb2c7d38a4605633956c02d5138e538c535ab
Jose <avina007@cougars.csusm.edu>
2020-04-19 22:36:27 -0700

    added main menu

---
+++ Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MainMenuButtons : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        Application.LoadLevel(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void MainMenu()
+    {
+        Application.LoadLevel(0);
+    }
+}
