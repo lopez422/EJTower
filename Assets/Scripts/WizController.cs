c9f856efb90bb58d181e3215a286dd5ddd2d412b
Eduardo <lopez422@icloud.com>
2020-04-30 16:25:42 -0700

    shooting, enemyDestroy, impactEffect

--- Assets/Scripts/WizController.cs
+++ Assets/Scripts/WizController.cs
@@ -5,13 +5,21 @@ public class WizController : MonoBehaviour
 {
 
 	private Transform target;
-	public float range = 15f;
 
+    [Header("Attributes")]
+	public float range = 15f;
+    public float fireRate = 1f;
+    private float fireCountdown = 0f;
+    
+
+    [Header("Setup Fields")]
+    public Transform partToRotate;
+    public float turnSpeed = 10f;
 	public string enemyTag = "enemy";
 
-	public Transform partToRotate;
+    public GameObject bulletPrefab;
+    public Transform firePoint;
 
-	public float turnSpeed = 10f;
     // Start is called before the first frame update
     void Start()
     {
@@ -57,7 +65,24 @@ public class WizController : MonoBehaviour
         Quaternion lookRotation = Quaternion.LookRotation(dir);
         Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
         partToRotate.rotation = Quaternion.Euler(-90f, rotation.y, 0f);
-        
+     
+        if(fireCountdown <= 0f)
+        {
+            Shoot();
+            fireCountdown = 1f/ fireRate;
+        }   
+
+        fireCountdown -= Time.deltaTime;
+
+    }
+
+    void Shoot()
+    {
+        GameObject magicGo = (GameObject)Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        MagicShot magicShot = magicGo.GetComponent<MagicShot>();
+
+        if(magicShot != null)
+            magicShot.Seek(target); 
     }
 
     void onDrawGizmosSelected()
