23abafb8568a0c305348dc7011c18ef2cc8ec66d
Eduardo <lopez422@icloud.com>
2020-04-30 18:32:59 -0700

    cannon placement after game is started

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,35 @@
+﻿
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+
+	public static BuildManager instance;
+
+	void Awake()
+	{
+		if(instance != null)
+		{
+			Debug.LogError("More than 1 BuildManager in scene!");
+			return;
+		}
+		instance = this;
+
+	}
+
+	public GameObject cannonPrefab;
+
+	void Start()
+	{
+		turretToBuild = cannonPrefab;
+	}
+
+    private GameObject turretToBuild;
+
+
+
+    public GameObject GetTurretToBuild()
+    {
+    	return turretToBuild;
+    }
+}
--- Assets/Scripts/CannonController.cs
+++ Assets/Scripts/CannonController.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 public class CannonController : MonoBehaviour
 {
 
-	private Transform target;
-	public float range = 15f;
+	   private Transform target;
 
-	public string enemyTag = "enemy";
+    [Header("Attributes")]
+    public float range = 15f;
+    public float fireRate = 1f;
+    private float fireCountdown = 0f;
+    
 
-	public Transform partToRotate;
+    [Header("Setup Fields")]
+    public Transform partToRotate;
+    public float turnSpeed = 10f;
+    public string enemyTag = "enemy";
 
-	public float turnSpeed = 10f;
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,29 +28,29 @@ public class CannonController : MonoBehaviour
 
     void UpdateTarget()
     {
-    	GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
 
-    	float shortestDistance = Mathf.Infinity;
-    	GameObject nearestEnemy = null;
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
 
-    	foreach(GameObject enemy in enemies)
-    	{
-    		float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
-    		if(distanceToEnemy < shortestDistance)
-    		{
-    			shortestDistance = distanceToEnemy;
-    			nearestEnemy = enemy;
-    		}
-    	}
+        foreach(GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if(distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
 
-    	if(nearestEnemy != null && shortestDistance <= range)
-    	{
-    		target = nearestEnemy.transform;
-    	}
-    	else 
-    	{
-    		target = null;
-    	}
+        if(nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        }
+        else 
+        {
+            target = null;
+        }
     }
 
     // Update is called once per frame
@@ -50,19 +58,36 @@ public class CannonController : MonoBehaviour
     {
         if(target == null)
         {
-        	return;
+            return;
         }
 
         Vector3 dir = target.position - transform.position;
         Quaternion lookRotation = Quaternion.LookRotation(dir);
         Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
         partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
-        
+     
+        if(fireCountdown <= 0f)
+        {
+            Shoot();
+            fireCountdown = 1f/ fireRate;
+        }   
+
+        fireCountdown -= Time.deltaTime;
+
+    }
+
+    void Shoot()
+    {
+        GameObject magicGo = (GameObject)Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        MagicShot magicShot = magicGo.GetComponent<MagicShot>();
+
+        if(magicShot != null)
+            magicShot.Seek(target); 
     }
 
     void onDrawGizmosSelected()
     {
-    	Gizmos.color = Color.blue;
-    	Gizmos.DrawWireSphere(transform.position, range);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, range);
     }
 }
